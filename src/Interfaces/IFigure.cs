96a6871ab8b497bbf680e5af883013de8ad9c3b0
albert-and-ik <113927533+albert-and-ik@users.noreply.github.com>
2022-09-21 14:44:03 +0300

    Update IFigure.cs

--- src/Interfaces/IFigure.cs
+++ src/Interfaces/IFigure.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 namespace Figures.Interfaces
 {
     /// <summary>
-    /// Определяет фигуру, которая должна реализовывать <see cref="IPerimeterable"/> и <see cref="IAreal"/>
+    /// Определяет общий интерфейс для фигур, который должн реализовывать <see cref="IPerimeterable"/> и <see cref="IAreal"/>
     /// </summary>
     public interface IFigure : IPerimeterable, IAreal
     {
